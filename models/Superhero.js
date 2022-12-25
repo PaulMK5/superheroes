b37736530162e96ee242611d6e223e506a71de0b
Paul <pavelmkdev@gmail.com>
2022-12-26 00:23:19 +0200

    belong to many association added

--- models/Superhero.js
+++ models/Superhero.js
@@ -1,17 +1,20 @@
 'use strict';
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Superheroes extends Model {
+  class Superhero extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Superhero.belongsToMany(models.Superpower, {
+        through: 'heroes_powers',
+        foreignKey: 'heroId'
+      });
     }
   }
-  Superheroes.init(
+  Superhero.init(
     {
       nickname: {
         type: DataTypes.STRING,
@@ -46,5 +49,5 @@ module.exports = (sequelize, DataTypes) => {
       underscored: true
     }
   );
-  return Superheroes;
+  return Superhero;
 };
