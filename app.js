e2cd5a65ae9d87a053c6ebc9dc015fdfdb8eb3b2
Paul <pavelmkdev@gmail.com>
2022-12-25 23:58:20 +0200

    app.js clear

--- app.js
+++ app.js
@@ -2,18 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const router = require('./routes');
 // const { errorHandler } = require('./errorHandler');
-// const { STATIC_PATH } = require('./config/path.config');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-// app.use(express.static(STATIC_PATH));
 app.use('/api', router);
 // app.use(errorHandler);
-
-app.get('/', (req, res, next) => {
-  console.log('hello');
-});
 
 module.exports = app;
