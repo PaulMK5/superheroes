f43923f185dd8e3ef441add5a6fa6d115d091196
Paul <pavelmkdev@gmail.com>
2022-12-29 20:43:27 +0200

    task done

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const router = require('./routes');
 const { STATIC_PATH } = require('./config/path.config');
-// const { errorHandler } = require('./errorHandler');
 
 const app = express();
 
@@ -10,6 +9,5 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(STATIC_PATH));
 app.use('/api', router);
-// app.use(errorHandler);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 const { Router } = require('express');
 const heroRouter = require('./heroRouter');
-const powerRouter = require('./powerRouter');
 
 const router = Router();
 
 router.use('/heroes', heroRouter);
-// router.use('/powers', powerRouter);
 
 module.exports = router;
